8de15fe21dff3d1386f7b6615dfba056f9a50440
guilherme luiz <guilhermebluiz@hotmail.com>
2022-01-19 20:11:47 -0300

    Ensure if schedule time is on the available window

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -6,9 +6,20 @@ class Schedule < ApplicationRecord
   private
 
   def schedule_time_validation
-    if Schedule.where('? >= begin_time AND ? < end_time AND room_id = ?', begin_time, begin_time, room_id).present? ||
-       Schedule.where('? > begin_time AND ? <= end_time AND room_id = ?', end_time, end_time, room_id).present?
-      errors.add(:schedule_time, 'has been already taken')
+    if schedule_time_already_taken? && available_schedule_time_window?
+      errors.add(:schedule_time, 'is unavailable')
     end
+  end
+
+  def schedule_time_already_taken?
+    Schedule.where('? >= begin_time AND ? < end_time AND room_id = ?', begin_time, begin_time, room_id).present? ||
+      Schedule.where('? > begin_time AND ? <= end_time AND room_id = ?', end_time, end_time, room_id).present?
+  end
+
+  def available_schedule_time_window?
+    return false unless begin_time.on_weekday? && end_time.on_weekday?
+
+    begin_time >= begin_time.beginning_of_day + 6.hours &&
+      end_time <= end_time.beginning_of_day + 15.hours
   end
 end
