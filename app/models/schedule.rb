63868dd7b6fb0c7c3aef097531b769161b98dd83
guilherme luiz <guilhermebluiz@hotmail.com>
2022-01-19 18:45:23 -0300

    Add schedule time validation to schedule module

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,14 @@
 class Schedule < ApplicationRecord
   belongs_to :room
+  validates :begin_time, :end_time, presence:true
+  validate :schedule_time_validation
+
+  private
+
+  def schedule_time_validation
+    if Schedule.where('? >= begin_time AND ? < end_time AND room_id = ?', begin_time, begin_time, room_id).present? ||
+       Schedule.where('? > begin_time AND ? <= end_time room_id = ?', end_time, end_time, room_id).present?
+      errors.add(:schedule_time, 'has been already taken')
+    end
+  end
 end
